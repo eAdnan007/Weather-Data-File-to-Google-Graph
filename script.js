3d565772c0a90151aafafd5c31f2d2d68d4e78bf
Mohaimenul Adnan <adnan007.id@gmail.com>
2016-02-20 21:09:59 +0000

    Chart made function ready.

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 google.charts.load('current', {packages: ['corechart', 'line']});
-google.charts.setOnLoadCallback(drawBackgroundColor);
+google.charts.setOnLoadCallback(function(){
+    drawBackgroundColor(weather_data.temparature, 'Temperature', 'chart_div');
+});
 
-function drawBackgroundColor() {
-      var data = new google.visualization.DataTable();
-      data.addColumn('timeofday', 'X');
-      data.addColumn('number', 'Temperature');
+function drawBackgroundColor(rows, legend, el_id) {
+  var data = new google.visualization.DataTable();
+  data.addColumn('timeofday', 'X');
+  data.addColumn('number', legend);
 
-      data.addRows( weather_data.temparature );
+  data.addRows( rows );
 
-      var options = {
-        hAxis: {
-          title: 'Time'
-        },
-        vAxis: {
-          title: 'Level'
-        },
-        backgroundColor: '#f1f8e9'
-      };
+  var options = {
+    hAxis: {
+      title: 'Time'
+    },
+    vAxis: {
+      title: 'Level'
+    },
+    backgroundColor: '#f1f8e9'
+  };
 
-      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-      chart.draw(data, options);
-    }
+  var chart = new google.visualization.LineChart(document.getElementById(el_id));
+  chart.draw(data, options);
+}
